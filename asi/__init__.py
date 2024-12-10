7e8001e1da17c21569d3343e61cd7e539badda82
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2024-12-10 14:40:51 +0200

    add white correction

--- asi/__init__.py
+++ asi/__init__.py
@@ -1,6 +1,7 @@
 from .draw import draw_multi_crosss, reconstruct_rgb
 from .envi import parse_envi_header
 from .io import load_spectral_image
+from .preprocess import white_correction
 from .utils import get_wavelengths
 
 __all__ = [
@@ -9,4 +10,5 @@ __all__ = [
     "load_spectral_image",
     "parse_envi_header",
     "reconstruct_rgb",
+    "white_correction",
 ]
